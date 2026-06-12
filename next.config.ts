0eb0e74501f7806a63ac2d2f4faf7a26aa31718f
Veronelli <facu2000veronelli@outlook.com>
2026-06-12 06:08:48 +0100

    add prefix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "./",
   trailingSlash: true,
   allowedDevOrigins: ['fhome'],
+  basePath: '/curriculum_next',
+  assetPrefix: '/curriculum_next',
 }
 
 export default nextConfig
